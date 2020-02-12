6ac5504cd6aca3f48c17a9173727dbc78cfdc70a
Roger Wang <rogerwangcs@gmail.com>
2020-02-12 14:50:26 -0500

    add live update fetching

--- js/main.js
+++ js/main.js
@@ -1,56 +1,86 @@
 var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
+  lFollowY = 0,
+  x = 0,
+  y = 0,
+  friction = 1 / 30;
 
 function moveBackground() {
-    x += (lFollowX - x) * friction;
-    y += (lFollowY - y) * friction;
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
 
-    translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.2)';
+  translate = "translate(" + x + "px, " + y + "px) scale(1.2)";
 
-    $('.move').css({
-        '-webit-transform': translate,
-        '-moz-transform': translate,
-        'transform': translate
-    });
+  $(".move").css({
+    "-webit-transform": translate,
+    "-moz-transform": translate,
+    transform: translate
+  });
 
-    window.requestAnimationFrame(moveBackground);
+  window.requestAnimationFrame(moveBackground);
 }
 
-$(window).on('mousemove', function (e) {
-    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-    lFollowX = (30 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-    lFollowY = (30 * lMouseY) / 100;
+$(window).on("mousemove", function(e) {
+  var lMouseX = Math.max(
+    -100,
+    Math.min(100, $(window).width() / 2 - e.clientX)
+  );
+  var lMouseY = Math.max(
+    -100,
+    Math.min(100, $(window).height() / 2 - e.clientY)
+  );
+  lFollowX = (30 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (30 * lMouseY) / 100;
 
-    moveBackground();
+  moveBackground();
 });
 
+$(window).on("load", function() {
+  let websiteData =
+    "https://spreadsheets.google.com/feeds/cells/18nBSXeKWQJMeaQ6TlUxtGQHrhtY-KwKrWTRuhaHSWy0/1/public/full?alt=json";
+  fetch(websiteData)
+    .then(response => {
+      return response.json();
+    })
+    .then(myJson => {
+      let announcements = myJson.feed.entry;
 
+      let an = document.getElementById("announcements");
+      announcements.forEach(function(message) {
+        let contents = message.content.$t.split(";");
 
-$(window).on("load", function () {
-    $(window).scroll(function () {
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        $(".fade").each(function () {
-            /* Check the location of each desired element */
-            var objectTop = $(this).offset().top;
+        let li = document.createElement("li");
+        li.innerHTML = "[" + contents[0] + "] ";
+        li.innerHTML += contents[1] + ": ";
+        li.innerHTML += contents[2];
+        an.appendChild(li);
+        // console.log(contents);
+      });
+      //   console.log(announcements);
+    });
 
-            /* If the element is completely within bounds of the window, fade it in */
-            if (objectTop < windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css("opacity") == 0) {
-                    $(this).fadeTo(100, 1);
-                }
-            } else { //object goes out of view (scrolling up)
-                if ($(this).css("opacity") == 1) {
-                    $(this).fadeTo(500, 0);
-                }
-            }
-        });
-    }).scroll(); //invoke scroll-handler on page-load
+  $(window)
+    .scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".fade").each(function() {
+        /* Check the location of each desired element */
+        var objectTop = $(this).offset().top;
+
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectTop < windowBottom) {
+          //object comes into view (scrolling down)
+          if ($(this).css("opacity") == 0) {
+            $(this).fadeTo(100, 1);
+          }
+        } else {
+          //object goes out of view (scrolling up)
+          if ($(this).css("opacity") == 1) {
+            $(this).fadeTo(500, 0);
+          }
+        }
+      });
+    })
+    .scroll(); //invoke scroll-handler on page-load
 });
-
 
 // var circles = [];
 // let numCirc = 50;
