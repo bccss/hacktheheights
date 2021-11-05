97841523fb16dca0355ad4b27ef2a630503a97d1
kennywang01 <73405958+kennywang01@users.noreply.github.com>
2021-11-04 22:17:14 -0400

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 5, 2021 18:00:00").getTime();
+var countDownDate = new Date("Nov 6, 2021 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
