5d89d2bd12dcb54657cbdc376a0edd939fa1e124
scheppatphoto <schepphoto@users.noreply.github.com>
2023-08-04 23:15:33 -0700

    Add FAQ & Events sections
    implemented in react, minor work on spacing/colors/format needed

--- src/sections/Container.jsx
+++ src/sections/Container.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
 // import Button from '@mui/material/Button';
-import { AppBar, Container } from '@mui/material'
-import Header from './Header';
-import { LandingSplash, About, Team, FAQ, Sponsors, PhotoCarousel, Footer} from './index.js';
+import { AppBar, Container } from "@mui/material";
+import Header from "./Header";
+import {
+  LandingSplash,
+  About,
+  Team,
+  FAQ,
+  Events,
+  Sponsors,
+  PhotoCarousel,
+  Footer,
+} from "./index.js";
 
 export const Home = () => {
   return (
     <div>
       <Header />
-      <Container sx={{ bgcolor: '#F6EEE3', height: '100vh' }}>
+      <Container sx={{ bgcolor: "#F6EEE3", height: "100vh" }}>
         <LandingSplash />
         <About />
         <Team />
         <FAQ />
+        <Events />
         <Sponsors />
         <PhotoCarousel />
       </Container>
       <Footer />
     </div>
-  )
-}
+  );
+};
--- src/sections/FAQ.jsx
+++ src/sections/FAQ.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./FAQ.css";
 
-const FAQ = () => {
+const dataCollection = [
+  {
+    question: "What is Hack The Heights (HTH)?",
+    answer:
+      "HTH will be a thrilling 24-hour event held on November 13th, 2023 at Boston College.  Admission is free and includes mentors, workshops, merch, food, and a fun experience!",
+  },
+  {
+    question: "How do I and or my team qualify?",
+    answer:
+      "Any student at Boston College or the Boston area is welcome to join us! Teams can have a maximum of four people. You can either register with your team using the form or create one during the event!",
+  },
+  {
+    question: "Where do I sleep?",
+    answer:
+      "While coding can be fun, we want you to get some rest when needed! Sleeping bags will be provided by our HTH students. ",
+  },
+  {
+    question: "Can I volunteer to be a mentor?",
+    answer:
+      "Yes! We are always looking for mentors to help our hackers. If you are interested, please fill out the form on our website. ",
+  },
+  {
+    question: "What project should I make?",
+    answer:
+      "We have exciting themes for your projects, and the top three teams will win a prize. Don't feel any pressure to make a perfect project, submission is totally optional!",
+  },
+];
+
+function FAQ() {
+  const [accordion, setActiveAccordion] = useState(-1);
+
+  function toggleAccordion(index) {
+    if (index === accordion) {
+      setActiveAccordion(-1);
+      return;
+    }
+    setActiveAccordion(index);
+  }
+
   return (
-    <div>FAQ</div>
-  )
+    <>
+      <div className="container">
+        <div>
+          <div className="accordion__title">FAQ</div>
+        </div>
+        <div className="accordion__faq">
+          {dataCollection.map((item, index) => (
+            <div key={index} onClick={() => toggleAccordion(index)}>
+              <div className="accordion__faq-heading">
+                <h3 className={accordion === index ? "active" : ""}>
+                  {item.question}
+                </h3>
+                <div>
+                  {accordion === index ? (
+                    <span className="verticle">-</span>
+                  ) : (
+                    <span className="horizental">+</span>
+                  )}
+                </div>
+              </div>
+              <div>
+                <p className={accordion === index ? "active" : "inactive"}>
+                  {item.answer}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default FAQ
+export default FAQ;
