08aea2464c0d379c22749155d258025aa4d9310d
Hunter S <hunterschep@gmail.com>
2023-10-02 20:56:50 -0400

    fix image carousel
    make the images render

--- src/sections/components/PhotoCarousel.jsx
+++ src/sections/components/PhotoCarousel.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
-
 import "../stylesheets/PhotoCarousel.css";
 
-const PhotoCarousel = ({ data }) => {
+const PhotoCarousel = () => {
   const [slide, setSlide] = useState(0);
 
+  const slides = [
+    { "src": "/images/IMG_5235.jpg", "alt": "Image 1" },
+    { "src": "/images/IMG_5235.jpg", "alt": "Image 2" },
+    { "src": "/images/IMG_5251.jpg", "alt": "Image 3" },
+    { "src": "/images/IMG_5258.jpg", "alt": "Image 4" },
+    { "src": "/images/IMG_5270.jpg", "alt": "Image 5" },
+    { "src": "/images/IMG_5279.jpg", "alt": "Image 6" }
+  ];
+
   const nextSlide = () => {
-    setSlide(slide === data.slides.length - 1 ? 0 : slide + 1);
+    setSlide(slide === slides.length - 1 ? 0 : slide + 1);
   };
 
   const prevSlide = () => {
-    setSlide(slide === 0 ? data.slides.length - 1 : slide - 1);
+    setSlide(slide === 0 ? slides.length - 1 : slide - 1);
   };
 
   return (
@@ -19,14 +27,14 @@ const PhotoCarousel = ({ data }) => {
       <h2 className="title">PAST EVENTS</h2> 
       <div className="carousel">
         <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide} />
-        {data.slides.map((item, idx) => {
+        {slides.map((item, idx) => {
           return (
-            <img src={item.src} alt={item.alt} key={idx} className={slide === idx ? "slide" : "slide-hidden"} />
+            <img src={process.env.PUBLIC_URL + item.src} alt={item.alt} key={idx} className={slide === idx ? "slide" : "slide-hidden"} />
           );
         })}
         <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide} />
         <span className="indicators">
-          {data.slides.map((_, idx) => {
+          {slides.map((_, idx) => {
             return (
               <button
                 key={idx}
