aa23a36e019dce99127c291c8b74f7aa80342f02
Hunter S <hunterschep@gmail.com>
2023-11-04 11:15:11 -0400

    fix events

--- src/sections/components/Events.jsx
+++ src/sections/components/Events.jsx
@@ -42,6 +42,8 @@ function Events() {
     }
   ]);
 
+  const scheduleImagePath = "/images/schedule.png";
+
   return (
     <>
       <div>
@@ -49,12 +51,29 @@ function Events() {
           <div className="container" id="Events">
             <h1>EVENTS</h1>
             <div className="cards">
-              {/* ...card mapping */}
+              {cards.map((card, index) => (
+                <div key={index} className="card">
+                  <div className="card-header">
+                    <div className="card-details">
+                      <h3>{card.title}</h3>
+                      <p>{card.time}</p>
+                    </div>
+                    <div className="card-image">
+                      <img src={card?.imgSrc} alt={card.title} />
+                    </div>
+                  </div>
+                  <div className="card-content">
+                    <p></p>
+                    <p>{card.text}</p>
+                  </div>
+                  {/*{<button className="btn">Learn More</button>}*/}
+                </div>
+              ))}
             </div>
             <div className='schedule-container'>
-              {/* Link to the Google Sheets document */}
-              <a href="https://docs.google.com/spreadsheets/d/1SGdaSd_EOvHjq_LrMRlpMnCiedcnTcd_mW0Z699jW1U/edit#gid=0" target="_blank" rel="noopener noreferrer" className='schedule-download'>
-                View Schedule
+              {/* Link that opens the schedule image in a new tab */}
+              <a href={scheduleImagePath} target="_blank" rel="noopener noreferrer" className='schedule-link'>
+                <h1>Schedule</h1>
               </a>
             </div>
           </div>
